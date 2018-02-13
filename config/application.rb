9e8e66eb9df0f7aedee37e3129ff3832b7d743db
Akshay Mittal <akshaycanodia@gmail.com>
2018-02-13 16:14:08 -0600

    Configure and create helpers - Change include_all elpers config to false - Split the _collapsible_elements.html.erb files content into partials and extract logic from the file in partials

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module FolkshubRor
   class Application < Rails::Application
+    config.action_controller.include_all_helpers = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
