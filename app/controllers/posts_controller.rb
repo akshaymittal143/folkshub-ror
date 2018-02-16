2b7f0b84056999bc847991d888fb45510aaac365
Akshay Mittal <akshaycanodia@gmail.com>
2018-02-16 15:36:09 -0600

    Create a UI to create new posts - Inside the PostsController:   define new and create actions   define a post_params method   define a before_action filter - Inside the ApplicationController:   define a redirect_if_not_signed_in method   define a redirect_if_signed_in method - Create a new template for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,25 @@
 class PostsController < ApplicationController
+    before_action :redirect_if_not_signed_in, only: [:new]
+    
     def show
         @post = Post.find(params[:id])        
     end
     
+    def new
+        @branch = params[:branch]
+        @categories = Category.where(branch: @branch)
+        @post = Post.new
+      end
+    
+      def create
+        @post = Post.new(post_params)
+        if @post.save 
+          redirect_to post_path(@post) 
+        else
+          redirect_to root_path
+        end
+      end
+
     def hobby
         posts_for_branch(params[:action])
       end
@@ -16,7 +33,12 @@ class PostsController < ApplicationController
       end
 
       private
-      
+
+      def post_params
+        params.require(:post).permit(:content, :title, :category_id)
+                             .merge(user_id: current_user.id)
+      end
+
       def posts_for_branch(branch)
         @categories = Category.where(branch: branch)
         @posts = get_posts.paginate(page: params[:page])
@@ -24,9 +46,9 @@ class PostsController < ApplicationController
             format.html
             format.js { render partial: 'posts/posts_pagination_page' }
           end
-    end
+        end
       
-    def get_posts
+     def get_posts
         PostsForBranchService.new({
           search: params[:search],
           category: params[:category],
