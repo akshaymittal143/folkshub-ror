7c06964f94b998d5c47d2a7cde3d4d3c12d91e19
Akshay Mittal <akshaycanodia@gmail.com>
2018-02-16 17:23:24 -0600

    Create Private::Conversation and Private::Message models - Define associations between User, Private::Conversation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215163918) do
+ActiveRecord::Schema.define(version: 20180216231320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,27 @@ ActiveRecord::Schema.define(version: 20180215163918) do
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
+  create_table "private_conversations", force: :cascade do |t|
+    t.integer  "recipient_id"
+    t.integer  "sender_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["recipient_id", "sender_id"], name: "index_private_conversations_on_recipient_id_and_sender_id", unique: true, using: :btree
+    t.index ["recipient_id"], name: "index_private_conversations_on_recipient_id", using: :btree
+    t.index ["sender_id"], name: "index_private_conversations_on_sender_id", using: :btree
+  end
+
+  create_table "private_messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "conversation_id"
+    t.boolean  "seen",            default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.index ["conversation_id"], name: "index_private_messages_on_conversation_id", using: :btree
+    t.index ["user_id"], name: "index_private_messages_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name",                   default: "", null: false
     t.string   "email",                  default: "", null: false
@@ -49,4 +70,5 @@ ActiveRecord::Schema.define(version: 20180215163918) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "private_messages", "users"
 end
